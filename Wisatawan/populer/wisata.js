03a0b4217b7c973d7e22d77d754b5f36e91c0439
Apryadi D. Putra Tangalayuk <148511727+Apryadi@users.noreply.github.com>
2024-06-12 00:30:10 +0800

    Popular Page
    Masih polos, jaga" kalau ada masalah dan nd sengaja salah pencet

---
+++ Wisatawan/populer/wisata.js
@@ -0,0 +1,54 @@
+var loginText = document.getElementById('home-page');
+
+loginText.addEventListener('click', function() {
+    var overlay = document.createElement('div');
+    overlay.classList.add('overlay');
+    document.body.appendChild(overlay);
+    setTimeout(function() {
+        window.location.href = '../HomePage/homePage.html'
+    })
+})
+
+var loginText = document.getElementById('home-page-logo');
+
+loginText.addEventListener('click', function() {
+    var overlay = document.createElement('div');
+    overlay.classList.add('overlay');
+    document.body.appendChild(overlay);
+    setTimeout(function() {
+        window.location.href = '../HomePage/homePage.html'
+    })
+})
+
+var loginText = document.getElementById('populer');
+
+loginText.addEventListener('click', function() {
+    var overlay = document.createElement('div');
+    overlay.classList.add('overlay');
+    document.body.appendChild(overlay);
+    setTimeout(function() {
+        window.location.href = '../populer/populer.html'
+    })
+})
+
+var loginText = document.getElementById('tologin');
+
+loginText.addEventListener('click', function() {
+    var overlay = document.createElement('div');
+    overlay.classList.add('overlay');
+    document.body.appendChild(overlay);
+    setTimeout(function() { 
+        window.location.href = '../LoginPage/login-page.html'
+    })
+})
+
+var loginText = document.getElementById('gunung');
+
+loginText.addEventListener('click', function() {
+    var overlay = document.createElement('div');
+    overlay.classList.add('overlay');
+    document.body.appendChild(overlay);
+    setTimeout(function() {
+        window.location.href = '../gunung/gunung.html'
+    })
+})
