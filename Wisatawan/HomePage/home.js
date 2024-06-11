7e60e49fad0d3ea1646f3404a85172c671382c02
Apryadi D. Putra Tangalayuk <148511727+Apryadi@users.noreply.github.com>
2024-06-11 21:41:32 +0800

    Home Page
    Memiliki Tombol search Dan Logo, Navbar berfungsi, dan berisi beberapa informasi terkait wisata yang ingin dikunjungi
    
    Footer nya Belum 100% Selesai + Blum Masuk Color Pallette nya

---
+++ Wisatawan/HomePage/home.js
@@ -0,0 +1,65 @@
+var loginText = document.getElementById('home-page');
+
+loginText.addEventListener('click', function() {
+    var overlay = document.createElement('div');
+    overlay.classList.add('overlay');
+    document.body.appendChild(overlay);
+    setTimeout(function() {
+        window.location.href = '../HomePage/homePage.html'
+    })
+})
+
+var loginText = document.getElementById('populer');
+
+loginText.addEventListener('click', function() {
+    var overlay = document.createElement('div');
+    overlay.classList.add('overlay');
+    document.body.appendChild(overlay);
+    setTimeout(function() {
+        window.location.href = '../populer/populer.html'
+    })
+})
+
+var loginText = document.getElementById('tologin');
+
+loginText.addEventListener('click', function() {
+    var overlay = document.createElement('div');
+    overlay.classList.add('overlay');
+    document.body.appendChild(overlay);
+    setTimeout(function() {
+        window.location.href = '../LoginPage/login-page.html'
+    })
+})
+
+var loginText = document.getElementById('home-page-logo');
+
+loginText.addEventListener('click', function() {
+    var overlay = document.createElement('div');
+    overlay.classList.add('overlay');
+    document.body.appendChild(overlay);
+    setTimeout(function() {
+        window.location.href = '../HomePage/homePage.html'
+    })
+})
+
+var loginText = document.getElementById('gunung');
+
+loginText.addEventListener('click', function() {
+    var overlay = document.createElement('div');
+    overlay.classList.add('overlay');
+    document.body.appendChild(overlay);
+    setTimeout(function() {
+        window.location.href = '../gunung/gunung.html'
+    })
+})
+
+var loginText = document.getElementById('makassar');
+
+loginText.addEventListener('click', function() {
+    var overlay = document.createElement('div');
+    overlay.classList.add('overlay');
+    document.body.appendChild(overlay);
+    setTimeout(function() {
+        window.location.href = '../gunung/gunung.html'
+    })
+})
