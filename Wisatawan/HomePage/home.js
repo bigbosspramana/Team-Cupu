dd08e8aa6daa7ed61da3ede855d2da6a8429d755
Apryadi D. Putra Tangalayuk <148511727+Apryadi@users.noreply.github.com>
2024-06-14 22:56:38 +0800

    Update Home Page (Landing Page)
    # Merubah Tampilan Menjadi Lebih Indah dengan animasi dan tampilan yang baru.
    - Mengubah Gambar Dari Bagian Pertama yang di lihat user
    - Menambahkan Animasi Untuk informasi di bagian bawahnya
    - Gambar masih perlu di cari
    - Teks informasi masih kosong karena masih bingung ka mau baca apa

--- Wisatawan/HomePage/home.js
+++ Wisatawan/HomePage/home.js
@@ -1,65 +1,16 @@
-var loginText = document.getElementById('home-page');
+const boxes = document.querySelectorAll('.box');
 
-loginText.addEventListener('click', function() {
-    var overlay = document.createElement('div');
-    overlay.classList.add('overlay');
-    document.body.appendChild(overlay);
-    setTimeout(function() {
-        window.location.href = '../HomePage/homePage.html'
-    })
-})
+window.addEventListener('scroll', checkboxes);
+checkboxes();
 
-var loginText = document.getElementById('populer');
-
-loginText.addEventListener('click', function() {
-    var overlay = document.createElement('div');
-    overlay.classList.add('overlay');
-    document.body.appendChild(overlay);
-    setTimeout(function() {
-        window.location.href = '../populer/populer.html'
-    })
-})
-
-var loginText = document.getElementById('tologin');
-
-loginText.addEventListener('click', function() {
-    var overlay = document.createElement('div');
-    overlay.classList.add('overlay');
-    document.body.appendChild(overlay);
-    setTimeout(function() {
-        window.location.href = '../LoginPage/login-page.html'
-    })
-})
-
-var loginText = document.getElementById('home-page-logo');
-
-loginText.addEventListener('click', function() {
-    var overlay = document.createElement('div');
-    overlay.classList.add('overlay');
-    document.body.appendChild(overlay);
-    setTimeout(function() {
-        window.location.href = '../HomePage/homePage.html'
-    })
-})
-
-var loginText = document.getElementById('gunung');
-
-loginText.addEventListener('click', function() {
-    var overlay = document.createElement('div');
-    overlay.classList.add('overlay');
-    document.body.appendChild(overlay);
-    setTimeout(function() {
-        window.location.href = '../gunung/gunung.html'
-    })
-})
-
-var loginText = document.getElementById('makassar');
-
-loginText.addEventListener('click', function() {
-    var overlay = document.createElement('div');
-    overlay.classList.add('overlay');
-    document.body.appendChild(overlay);
-    setTimeout(function() {
-        window.location.href = '../gunung/gunung.html'
-    })
-})
+function checkboxes() {
+    const triggerBottom = window.innerHeight / 7 * 4;
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top;
+        if (boxTop < triggerBottom) {
+            box.classList.add('show');
+        } else {
+            box.classList.remove('show');
+        }
+    });
+}
