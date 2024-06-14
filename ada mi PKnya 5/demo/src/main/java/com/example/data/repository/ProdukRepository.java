d5b0e868e3d84043ed3cc52e93ba7554bc6456cf
Alvin Yuga Pramana <148262832+bigbosspramana@users.noreply.github.com>
2024-06-14 22:43:30 +0800

    Delete ada mi PKnya 5/demo directory

--- ada mi PKnya 5/demo/src/main/java/com/example/data/repository/ProdukRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.data.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.example.model.ProdukModel;
-
-@Repository
-public interface ProdukRepository extends JpaRepository<ProdukModel, Long> {
-}
